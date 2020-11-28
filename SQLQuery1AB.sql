271484d8189f5e096d0b0ace5dd414a5747f7a71
piyush12346789 <piyushgupta024@gmail.com>
2020-11-28 08:33:08 +0530

    [Piyush] Add: Using tool to fetch details of the person and performing several operations in that.

---
+++ SQLQuery1AB.sql
@@ -0,0 +1,11 @@
+create table AddressBook
+(
+    first_name varchar(50),
+    last_name varchar(50),
+    address varchar(100),
+    city varchar(30),
+    state varchar(30),
+    zip int,
+    phone_number varchar(13),
+    email varchar(50)
+);
